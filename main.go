8dca2ec6a3d0cf1c6402ea81dd13e3da01a86438
Marius <mm1000@gmx.net>
2026-02-27 15:20:10 +0100

    feat: added Go Iterator

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+var cars = []string{"VW", "Opel", "Porsche", "Ferrari"}
+
+func main() {
+	for i := 0; i < len(cars), i++ {
+		fmt.Println(cars[i])
+		}
+}
---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+        String cars[] = { "VW", "Opel", "Porsche", "Ferrari" };
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
