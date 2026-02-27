22fc8348732ae41c75a81d6df0958a30dbdc3e33
Marius <mm1000@gmx.net>
2026-02-27 14:57:35 +0100

    feat: Added Simple Code Snippet

--- Iterator.py
+++ Iterator.py
@@ -1,1 +1,12 @@
+autos = ["VW", "Opel", "Porsche", "Ferrari"]
 
+
+print("Welches Auto möchtest du haben")
+input = input()
+
+for i in autos:
+    if input == i:
+        print("Haben wir dieses Auto auf Lager")
+        break
+    else:
+        print("Leider haben wir dieses Auto nicht mehr auf lager ")
